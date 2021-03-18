5c51a112950d0887137dfc6380ac39efa66383b2
Damian Gul <damian.robert.gul@gmail.com>
2021-03-18 09:53:31 +0100

    add a little bit of styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { deleteTask, changeStatusTask } from "../redux/todoActions";
 import AddTask from "./AddTask";
 
+import "./css/buttons.css";
+
 function Task(props) {
   const style = {
     color: "red",
@@ -22,9 +24,15 @@ function Task(props) {
         <p>
           <strong style={important ? style : null}>{text}</strong> - do{" "}
           <span>{date} </span>
-          <button onClick={handleTaskDone}>Zostało zrobione</button>
-          <button onClick={handleEditTask}>Edytuj</button>
-          <button onClick={handleDeleteTask}>Usuń</button>
+          <button className="buttonDone" onClick={handleTaskDone}>
+            Zostało zrobione
+          </button>
+          <button className="buttonEdit" onClick={handleEditTask}>
+            Edytuj
+          </button>
+          <button className="buttonDelete" onClick={handleDeleteTask}>
+            Usuń
+          </button>
         </p>
         {switchEdit ? (
           <AddTask {...props.task} handleSwitch={handleEditTask} />
@@ -39,7 +47,9 @@ function Task(props) {
         <p>
           <strong>{text}</strong> (zrobić do {date})
           <br />- potwierdzenie wykonania {finish}
-          <button onClick={handleDeleteTask}>Usuń</button>
+          <button className="buttonDelete" onClick={handleDeleteTask}>
+            Usuń
+          </button>
         </p>
       </div>
     );
